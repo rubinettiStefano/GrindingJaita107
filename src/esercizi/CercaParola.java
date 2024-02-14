f2f2a7321adcd3617134c63bf672b140e3de957d
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-02-14 10:00:19 +0100

    v3

---
+++ src/esercizi/CercaParola.java
@@ -0,0 +1,17 @@
+package esercizi;
+
+public class CercaParola 
+{
+    public static void main(String[] args) {
+        
+        //ricevete una matrice di CARATTERI 5x5
+       
+        //obiettivo1-> stampare TROVATO se in orizzontale, in qualche riga
+        //è presente la parola CANE
+        //altrimenti stampare NON TROVATO
+
+        //obiettivo1-> stampare TROVATO se in orizzontale o VERTICALE
+        //è presente la parola CANE
+        //altrimenti stampare NON TROVATO
+    }
+}
